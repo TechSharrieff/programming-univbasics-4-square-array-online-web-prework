2524b3e22c99db40bf08dd1db27eb4b72ab24daf
Sharrieff ross <crross@dons.usfca.edu>
2019-09-17 21:59:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  numbers = [1,2,3]
+  square_array(numbers)
+  new_numbers = [9,10,16,25]
+  square_array(new_numbers)
+end
+
+  while array[numbers] do
+    puts array[numbers]
+    counter = 0
+  end
 end
